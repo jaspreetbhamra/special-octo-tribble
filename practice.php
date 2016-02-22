caadf669c7c53f503d036690cadb19a7a0f2524f
Jaspreet Kaur Bhamra <Jaspreet Kaur Bhamra>
2016-02-22 07:30:38 +0530

    Testing

---
+++ practice.php
@@ -0,0 +1,23 @@
+<html>
+
+	<head>
+
+		<title> Practice </title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+			$firstNumber = 10;
+			print($firstNumber.' is a number'. "<br>");
+			print('$firstNumber'. "<br>");
+			print("$firstNumber". "<br>");
+			
+
+		?>
+
+	</body>
+
+</html>
